78387a06e170c9310d33dd2fffe6d01133065b37
Mookineii <monyjk576@gmail.com>
2026-03-13 23:45:50 +0700

    add

--- Lab1/Ex7.cpp
+++ Lab1/Ex7.cpp
@@ -1,65 +1,95 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
 struct Book {
     int bookID;
     string bookISBN;
-    string bookTitles;
-    string pulishedYear;
+    string bookTitle;
+    int publishedYear;
     string authorName[5];
     int authorCount;
-    double price[];
-    void displayBookByISBN(Book books[], int size, string isbn){
-        for(int i=0; i<size; i++){
-            if(books[i].bookISBN == isbn){
-                cout<<"=================================================\n";
-                cout<<"Book's ID: "<<books[i].bookID<<endl;
-                cout<<"Book's ISBN: "<<books[i].bookISBN<<endl;
-                cout<<"Book's Pulished year: "<<books[i].pulishedYear<<endl;
-                cout<<"Book's author Name : ";
-                for(int j=0; j<books[i].authorCount; i++){
-                    cout<<books[j].authorName[j]<<" ";
-                }
-                cout<<"\nBook's price: "<<books[i].price[i]<<endl;
-                cout<<"=================================================\n";
-            }
-        }
-    }
-    void displayAllBooks(Book books[], int size){
-        for(int i =0 ; i< size; i++){
-            cout<<"=================================================\n";
-            cout<<"Book's ID: "<<books[i].bookID<<endl;
-                cout<<"Book's ISBN: "<<books[i].bookISBN<<endl;
-                cout<<"Book's Pulished year: "<<books[i].pulishedYear<<endl;
-                cout<<"Book's author Name : ";
-                for(int j=0; j<books[i].authorCount; i++){
-                    cout<<books[j].authorName[j]<<" ";
-                }
-                cout<<"\nBook's price: "<<books[i].price[i]<<" $"<<endl;
-                cout<<"=================================================\n";
-        }
-    }
-
+    double price;
 };
 
-int main(){
-    Book books[2];
-    for(int i=0 ; i<2; i++){
-        cout<<"Book's ID: ";cin>>books[i].bookID;
-        cout<<"Book's title: ";cin>>books[i].bookTitles;
-        cout<<"Book's published year: ";cin>>books[i].pulishedYear;
-        cout<<"Book's ISBN: ";cin>>books[i].bookISBN;
-        cout<<"Book's number of author: ";cin>>books[i].authorCount;
-        cout<<"Book's author: \n"; 
-        for(int j=0 ; j<books[i].authorCount;j++){
-            cin>>books[i].authorName[j];
+void displayBookByISBN(Book books[], int size, string isbn) {
+    bool found = false;
+
+    for (int i = 0; i < size; i++) {
+        if (books[i].bookISBN == isbn) {
+            cout << "=================================================\n";
+            cout << "Book ID: " << books[i].bookID << endl;
+            cout << "Book ISBN: " << books[i].bookISBN << endl;
+            cout << "Book Title: " << books[i].bookTitle << endl;
+            cout << "Published Year: " << books[i].publishedYear << endl;
+            cout << "Author Name: ";
+            for (int j = 0; j < books[i].authorCount; j++) {
+                cout << books[i].authorName[j] << " ";
+            }
+            cout << "\nBook Price: " << books[i].price << " $" << endl;
+            cout << "=================================================\n";
+            found = true;
         }
-        cout<<"Book's price: ";cin>>books[i].price[i];
     }
-    cout<<"Display all books: \n";
-    books->displayAllBooks(books,2);
+
+    if (!found) {
+        cout << "Book not found.\n";
+    }
+}
+
+void displayAllBooks(Book books[], int size) {
+    for (int i = 0; i < size; i++) {
+        cout << "=================================================\n";
+        cout << "Book ID: " << books[i].bookID << endl;
+        cout << "Book ISBN: " << books[i].bookISBN << endl;
+        cout << "Book Title: " << books[i].bookTitle << endl;
+        cout << "Published Year: " << books[i].publishedYear << endl;
+        cout << "Author Name: ";
+        for (int j = 0; j < books[i].authorCount; j++) {
+            cout << books[i].authorName[j] << " ";
+        }
+        cout << "\nBook Price: " << books[i].price << " $" << endl;
+        cout << "=================================================\n";
+    }
+}
+
+int main() {
+    Book books[2];
+
+    for (int i = 0; i < 2; i++) {
+        cout << "\nEnter information for Book " << i + 1 << endl;
+
+        cout << "Book ID: ";
+        cin >> books[i].bookID;
+
+        cout << "Book Title: ";
+        cin >> books[i].bookTitle;
+
+        cout << "Published Year: ";
+        cin >> books[i].publishedYear;
+
+        cout << "Book ISBN: ";
+        cin >> books[i].bookISBN;
+
+        cout << "Number of authors: ";
+        cin >> books[i].authorCount;
+
+        cout << "Author names:\n";
+        for (int j = 0; j < books[i].authorCount; j++) {
+            cin >> books[i].authorName[j];
+        }
+
+        cout << "Book Price: ";
+        cin >> books[i].price;
+    }
+
+    cout << "\nDisplay all books:\n";
+    displayAllBooks(books, 2);
+
     string search;
-    cout<<"Search ISBN for book: ";cin>>search;
-    books->displayBookByISBN(books,2,search);
+    cout << "\nSearch ISBN for book: ";
+    cin >> search;
+    displayBookByISBN(books, 2, search);
+
     return 0;
 }
